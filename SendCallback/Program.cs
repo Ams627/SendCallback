113415af3a32fba9b0bbfe1da2cd9f9d83359444
Adrian Sims <asims@parkeon.com>
2018-01-10 19:48:11 +0000

    Add project files.

---
+++ SendCallback/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendCallback
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var address = "192.168.201.107";
+                var port = 9999;
+                var stringToSend = "APPELSGR";
+
+                var client = new TcpClient(address, port);
+                var stream = client.GetStream();
+                var ba = Encoding.ASCII.GetBytes(stringToSend);
+                stream.Write(ba, 0, ba.Length);
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add SendCallback/Program.cs
+git add SendCallback/App.config
+git add SendCallback/Properties/AssemblyInfo.cs
+git add SendCallback/SendCallback.csproj
+git add SendCallback.sln
+git add .gitignore
